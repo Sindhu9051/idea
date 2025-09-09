ca45d25ab034361e9551a1c08230858fa69ec61d
sindhu kumar <sindhukumarfrontenddev@gmail.com>
2025-09-09 21:24:15 +0530

    Re-uploading project after deletion

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import AllCrs from './components/AllCrs';
@@ -10,31 +10,15 @@ import Footer from './components/Footer';
 
 import { HelmetProvider } from "react-helmet-async";
 
-
-
-
-
 import WebinarForm from './components/Webinar';
 import BootcampForm from './components/Bootcamp';
 import Courses from './components/Courses';
 import InternshipForm from './components/Internship';
-
-
-
-
-
 import PaymentPage from "./components/PaymentPage";
-
 
 import TermsAndConditions from './components/Terms&Conditions';
 import RefundPolicy from './components/Refund&Cancellation';
 import PrivacyPolicy from './components/Privacy&Policy';
-
-
-
-
-
-
 
 function App() {
   return (
@@ -47,14 +31,11 @@ function App() {
           <Route path="/verify-certificates" element={<VerifyCertificate />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
-
           <Route path="/webinar" element={<WebinarForm />} />
           <Route path="/bootcamp" element={<BootcampForm />} />
           <Route path="/courses" element={<Courses />} />
           <Route path="/internship" element={<InternshipForm />} />
-
           <Route path="/payment" element={<PaymentPage />} />
-
           <Route path="/terms" element={<TermsAndConditions />} />
           <Route path="/refund" element={<RefundPolicy />} />
           <Route path="/privacy" element={<PrivacyPolicy />} />
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import './index.css'; // if you have global styles
+import './index.css'; // global styles
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
